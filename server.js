86df4967b734adaab456348ee9ac26ebf12e002c
Hector Mauricio Muñoz Ordoñez <dev.mauricio.munoz@gmail.com>
2024-12-05 12:33:05 -0500

    Configuración inicial gestion de gastos

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware global
+app.use(cors());
+app.use(express.json());
+
+// Conexión a la base de datos
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Conexión exitosa a MongoDB"))
+  .catch((error) => console.error("Error conectando a MongoDB:", error));
+
+// Rutas
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando correctamente");
+});
+
+// Inicia el servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Servidor corriendo en http://localhost:${PORT}/`)
+);
