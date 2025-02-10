726cf324a6520419148a9cbd49577878014363d1
Hector Mauricio Muñoz Ordoñez <dev.mauricio.munoz@gmail.com>
2025-02-10 18:11:18 -0500

    Update User.js
    Corrección de hash

--- models/User.js
+++ models/User.js
@@ -18,12 +18,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// Encriptar la contraseña antes de guardar el usuario
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+Corrección de hash
 
 module.exports = mongoose.model("User", userSchema);
