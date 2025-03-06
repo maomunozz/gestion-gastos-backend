deeff1eb2df26938b74c02ae6604e7d2c4f7aaa1
Hector Mauricio Muñoz Ordoñez <dev.mauricio.munoz@gmail.com>
2025-03-06 14:24:57 -0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -59,7 +59,7 @@ router.post("/login", async (req, res) => {
       return res.status(401).json({ error: "Credenciales inválidas" });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
+      expiresIn: 30,
     });
     res.status(200).json({ token });
   } catch (error) {
