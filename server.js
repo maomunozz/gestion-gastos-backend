a883f61c87a3e2bb1c4c6844b1eab335d27c6657
Hector Mauricio Muñoz Ordoñez <dev.mauricio.munoz@gmail.com>
2025-03-06 15:04:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 // Middleware global
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // Conexión a la base de datos
